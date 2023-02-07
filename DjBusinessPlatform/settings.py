391bd782e016692d305fc72ec0c6dbe6318bb0d0
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-02-07 14:52:48 +0300

    установил статик директорию

--- DjBusinessPlatform/settings.py
+++ DjBusinessPlatform/settings.py
@@ -120,3 +120,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+
+# FIXTURE_DIRS = (
+#     os.path.join(BASE_DIR, 'fixtures'),
+# )
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
+# CART_SESSION_ID = 'cart'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
