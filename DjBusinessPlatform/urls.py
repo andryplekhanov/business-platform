7d6ec5352abef3afdac50472400a692d1166fff6
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-02-08 15:55:27 +0300

    доб интернационализацию

--- DjBusinessPlatform/urls.py
+++ DjBusinessPlatform/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from DjBusinessPlatform import settings
 
 urlpatterns = [
+    path('i18n', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
     path('', include('app_main.urls')),
     path('', include('app_users.urls')),
