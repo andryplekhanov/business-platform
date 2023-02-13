304cf7b42069fa9230346480b2a30669f5fe814f
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-02-13 12:54:20 +0300

    доб. реферер в админку

--- app_users/admin.py
+++ app_users/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
 from .models import CustomUser
 
 
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'full_name', 'is_active', 'is_verified', 'is_freelancer', 'can_invite_referrals', 'date_joined')
+    list_display = ('id', 'email', 'full_name', 'get_referer', 'is_verified', 'is_freelancer', 'can_invite_referrals', 'date_joined')
     list_filter = ('date_joined', 'is_staff', 'is_active', 'is_verified', 'is_freelancer', 'can_invite_referrals')
     fieldsets = (
         (None, {'fields': ('email', 'full_name', 'password', 'phone_number', 'avatar', 'date_joined')}),
@@ -33,6 +35,11 @@ class CustomUserAdmin(admin.ModelAdmin):
     def get_referrals(self, obj):
         return CustomUser.objects.filter(referer_id=obj.id).count()
     get_referrals.short_description = _('пригласил пользователей')
+
+    def get_referer(self, obj):
+        link = reverse("admin:app_users_customuser_change", args=[obj.referer_id])
+        return format_html('<a href="{}">{}</a>', link, obj.referer)
+    get_referer.short_description = _('реферер')
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
